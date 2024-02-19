d10518e04e9d9df71e25837c2a1c31392c3e1a41
Pathi14 <pathipayong@gmail.com>
2024-02-19 11:07:10 +0100

    Install package

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,39 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client'
+
+const client = new ApolloClient({
+  uri: 'https://rickandmortyapi.com/graphql',
+  cache: new InMemoryCache(),
+});
+
+// const client = ...
+
+client
+  .query({
+    query: gql`
+      query Character {
+        characters {
+          info {
+            count
+            pages
+            next
+            prev
+          }
+          results {
+            name
+            image
+          }
+        }
+      }
+    `,
+  })
+  .then((result) => console.log(result));
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+
